69d382ba07acf3c05de5c9bdfcc9f02473a1509b
Sabina Kazantseva <sabinakazantceva@gmail.com>
2023-10-10 11:10:10 -1000

    todo form

---
+++ src/AddTodoForm.js
@@ -0,0 +1,19 @@
+import React from "react";
+function AddTodoForm() {
+    // Add a multi-line return statement with JSX
+    return (
+        <form>
+            {/* Create a label element with text "Title" */}
+            <label htmlFor="todoTitle">Title</label>
+
+            {/* Create a text input element with id "todoTitle" */}
+            <input type="text" id="todoTitle" />
+
+            {/* Create a submit button element with text "Add" */}
+            <button type="submit">Add</button>
+        </form>
+    );
+}
+
+export default AddTodoForm;
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
+import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm'; // Import the AddTodoForm component
 
-const todoList = [
-  { id: 1, title: "complete assigment" },
-  { id: 2, title: "go for a walk" },
-  { id: 3, title: "go shopping" },
-];
 function App() {
   return (
     <div>
       <h1>Todo List</h1>
-      <ul>
-        {todoList.map((task) => {
-          return (
-            <li key={task.id}>
-              {task.title}
-            </li>
-          )
-        })}
-      </ul>
+
+      <TodoList />
+      {/* Use the AddTodoForm component below the heading */}
+      <AddTodoForm />
     </div>
   );
 }
