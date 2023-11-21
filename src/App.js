5ed36d68ac523b0daf31684e31c99c0aaca39e17
Sabina Kazantseva <sabinakazantceva@gmail.com>
2023-11-20 19:54:40 -0800

    Lesson 1.7: Update App component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,60 @@
-import TodoList from './TodoList';
-import AddTodoForm from './AddTodoForm'; // Import the AddTodoForm component
 import React, { useState, useEffect } from 'react';
+import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
-function useSemiPersistentState(key, initialState) {
-  const [state, setState] = useState(() => {
-    const savedState = localStorage.getItem(key);
-    return savedState ? JSON.parse(savedState) : initialState;
-  });
+function App() {
+  // Using useState and useEffect directly for persistence
+  const localStorageKey = 'savedTodoList';
+  const [todoList, setTodoList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(state));
-  }, [key, state]);
+    // Define a Promise to mimic asynchronous data fetching
+    const fetchData = new Promise((resolve, reject) => {
+      // Simulate a loading delay with setTimeout
+      setTimeout(() => {
+        // Resolve the Promise with an object containing the data
+        resolve({ data: { todoList: [] } }); // Update todoList default state here
+      }, 2000); // 2 seconds delay
+    });
 
-  return [state, setState];
-}
-function App() {
-
-  const [todoList, setTodoList] = useSemiPersistentState('savedTodoList', []);
+    // Use the Promise to set the state when the data is resolved
+    fetchData
+      .then((result) => {
+        setTodoList(result.data.todoList);
+        setLoading(false); // Set loading to false regardless of success or failure
+      })
+      .catch((error) => {
+        console.error("error fetching data:", error);
+        setLoading(false);
+      });
+  }, []); // Empty dependency list to run the effect only once
+  useEffect(() => {
+    // Update localStorage only when the asynchronous fetch is complete
+    if (!loading) {
+      localStorage.setItem(localStorageKey, JSON.stringify(todoList));
+    }
+  }, [loading, todoList, localStorageKey]);
 
   function addTodo(newTodo) {
     setTodoList([...todoList, newTodo]);
   }
-  const removeTodo = (id) => {
+
+  function removeTodo(id) {
     const updatedTodoList = todoList.filter((todo) => todo.id !== id);
     setTodoList(updatedTodoList);
-  };
+  }
 
   return (
-    <>
-      <div>
-        <h1>Todo List</h1>
-        <AddTodoForm onAddTodo={addTodo} />
-        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
-      </div>
-    </>
+    <div>
+      <h1>Todo List</h1>
+      <AddTodoForm onAddTodo={addTodo} />
+      {loading ? <p>Loading...</p> : <TodoList todoList={todoList} onRemoveTodo={removeTodo} />}
+
+      <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+
+    </div>
   );
 }
-export default App;
 
+export default App;
